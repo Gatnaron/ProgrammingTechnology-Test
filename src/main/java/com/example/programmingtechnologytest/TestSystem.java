67ea8f40facc59307d05d021c72fb78c35fa52e6
Gatnaron <oganyan.ivan2016@yandex.ru>
2024-04-19 19:16:15 +0300

    update test

--- src/main/java/com/example/programmingtechnologytest/TestSystem.java
+++ src/main/java/com/example/programmingtechnologytest/TestSystem.java
@@ -16,35 +16,46 @@ public class TestSystem {
 
         List<String[]> testCases = new ArrayList<>();
 
+        // Тесты для создания точек
         testCases.add(new String[]{"Test1", "10.0", "20.0", "BLACK", "*"});
+        testCases.add(new String[]{"Test2", "30.0", "40.0", "RED", "#"});
 
-        testCases.add(new String[]{"Test2", "10.0", "20.0", "BLACK", "*", "30.0", "40.0"});
+        // Тесты для изменения координат точек
+        testCases.add(new String[]{"Test3", "50.0", "60.0", "GREEN", "!"});
+        testCases.add(new String[]{"Test4", "70.0", "80.0", "BLUE", "@"});
 
-        testCases.add(new String[]{"Test3", "10.0", "20.0", "BLACK", "*", "30.0", "40.0", "WHITE"});
+        // Тесты для изменения цвета точек
+        testCases.add(new String[]{"Test5", "90.0", "100.0", "YELLOW", "$"});
+        testCases.add(new String[]{"Test6", "110.0", "120.0", "CYAN", "%"});
 
-        testCases.add(new String[]{"Test4", "10.0", "20.0", "BLACK", "*", null});
+        // Тесты для удаления точек
+        testCases.add(new String[]{"Test7", "130.0", "140.0", "MAGENTA", "^"});
+        testCases.add(new String[]{"Test8", "150.0", "160.0", "ORANGE", "&"});
 
         return testCases.toArray(new String[0][]);
     }
 
     public static void solve(String[][] tests) {
+        List<MPoint> points = new ArrayList<>();
         for (String[] test : tests) {
             MPoint point = new MPoint(test[0], Double.parseDouble(test[1]), Double.parseDouble(test[2]),
                     Color.web(test[3]), test[4].charAt(0));
-
-            if (test.length >= 6 && test[5] != null) {
-                point.setX(Double.parseDouble(test[5]));
-                point.setY(Double.parseDouble(test[6]));
-            }
-
-            if (test.length >= 8 && test[7] != null) {
-                point.setColor(Color.web(test[7]));
-            }
-
-            if (test.length >= 9 && test[8] == null) {
-                point = null;
-            }
+            points.add(point);
         }
+
+        // Изменение координат точек
+        for (MPoint point : points) {
+            point.setX(point.getX() + 10);
+            point.setY(point.getY() + 10);
+        }
+
+        // Изменение цвета точек
+        for (MPoint point : points) {
+            point.setColor(Color.PINK);
+        }
+
+        // Удаление
+        points.removeIf(point -> point.getName().equals("Test7") || point.getName().equals("Test8"));
     }
 
     public static void check(String[][] tests) {
