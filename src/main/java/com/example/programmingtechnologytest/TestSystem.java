6725f8c91986df7a590713c91a832f2969d03a49
Gatnaron <oganyan.ivan2016@yandex.ru>
2024-04-19 18:03:57 +0300

    update test_system

--- src/main/java/com/example/programmingtechnologytest/TestSystem.java
+++ src/main/java/com/example/programmingtechnologytest/TestSystem.java
@@ -16,27 +16,42 @@ public class TestSystem {
 
         List<String[]> testCases = new ArrayList<>();
 
-        // Тесты для создания объекта MPoint
         testCases.add(new String[]{"Test1", "10.0", "20.0", "BLACK", "*"});
-        testCases.add(new String[]{"Test2", "40.0", "50.0", "WHITE", "#"});
+
+        testCases.add(new String[]{"Test2", "10.0", "20.0", "BLACK", "*", "30.0", "40.0"});
+
+        testCases.add(new String[]{"Test3", "10.0", "20.0", "BLACK", "*", "30.0", "40.0", "WHITE"});
+
+        testCases.add(new String[]{"Test4", "10.0", "20.0", "BLACK", "*", null});
+
         return testCases.toArray(new String[0][]);
     }
 
     public static void solve(String[][] tests) {
-        // Реализация эталонного решения
         for (String[] test : tests) {
             MPoint point = new MPoint(test[0], Double.parseDouble(test[1]), Double.parseDouble(test[2]),
                     Color.web(test[3]), test[4].charAt(0));
+
+            if (test.length >= 6 && test[5] != null) {
+                point.setX(Double.parseDouble(test[5]));
+                point.setY(Double.parseDouble(test[6]));
+            }
+
+            if (test.length >= 8 && test[7] != null) {
+                point.setColor(Color.web(test[7]));
+            }
+
+            if (test.length >= 9 && test[8] == null) {
+                point = null;
+            }
         }
     }
 
     public static void check(String[][] tests) {
-        // Проверка результатов
         for (String[] test : tests) {
             MPoint point = new MPoint(test[0], Double.parseDouble(test[1]), Double.parseDouble(test[2]),
                     Color.web(test[3]), test[4].charAt(0));
 
-            // Сравнение результатов с ожидаемыми значениями
             if (point != null) {
                 System.out.println("Test PASSED: " + test[0]);
             } else {
